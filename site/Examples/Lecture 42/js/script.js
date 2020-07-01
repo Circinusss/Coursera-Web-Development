45485195a1c0905aa700fe31ebff0539f7fd28e6
Chaitanya <saprechaitanya@gmail.com>
2020-07-01 22:48:23 +0530

    Updated

---
+++ site/Examples/Lecture 42/js/script.js
@@ -0,0 +1,14 @@
+var x;
+console.log();
+
+if (x==undefined){
+	console.log("x is undefined");
+}
+
+x = 5;
+if (x == undefined){
+	console.log("x is undefined");
+}
+else{
+	console.log("x is defined and is set to: " + x);
+}
