55e0f4e5de971e30943ab668d4299e3e8e53d82a
Chaitanya <saprechaitanya@gmail.com>
2020-07-17 18:43:05 +0530

    Updated

--- Assignments/Module 4/script.js
+++ Assignments/Module 4/script.js
@@ -1,11 +1,5 @@
 (function(){
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-// STEP 10:
-// Loop over the names array and say either 'Hello' or "Good Bye"
-// using the 'speak' method or either helloSpeaker's or byeSpeaker's
-// 'speak' method.
-// See Lecture 50, part 1
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0);
   if (firstLetter.toLowerCase() == 'j') {
---
+++ site/Examples/Lecture 58/js/script.js
@@ -0,0 +1,35 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var message = 
+                res.firstName + " " + res.lastName
+              if (res.likesFood) {
+                message += " likes food";
+              }
+              else {
+                message += " doesn't like food";
+              }
+              message += " and uses ";
+              message += res.numberOfDisplays + 1;
+              message += " displays for coding.";
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+            });
+      });
+  }
+);
+
+
+
+
+
